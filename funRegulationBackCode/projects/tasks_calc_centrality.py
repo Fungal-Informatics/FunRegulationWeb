636103226231784d69152c3533a385d9a1664360
GabrielNew <gabrielfai70@gmail.com>
2023-10-15 00:42:44 -0300

    Calcular centralidade pipeline completo

--- funRegulationBackCode/projects/tasks_calc_centrality.py
+++ funRegulationBackCode/projects/tasks_calc_centrality.py
@@ -21,8 +21,7 @@ def task_run_create_graph(self, registry_id, organism_accession):
     tfs = list()
     tgs = list()
 
-    #all_nodes = RegulatoryInteraction.objects.filter(organism_accession=organism_accession)
-    all_nodes = RegulatoryInteraction.objects.filter(organism_accession='GCA_001275765.2')
+    all_nodes = RegulatoryInteraction.objects.filter(organism_accession=organism_accession)
         
     for item in all_nodes:
         tfs.append(item.tf_locus_tag.locus_tag)
@@ -38,14 +37,16 @@ def task_degree_centrality(self, graph, registry_id, organism_accession):
     registry = ProjectAnalysisRegistry.objects.get(pk=registry_id)
     registry.task_calculate_degree = TaskResult.objects.get(task_id=self.request.id)
     registry.save()
-    logging.info('Running calculate degree centrality for organism %s' % organism_accession)
-    graph = json_graph.node_link_graph(graph)
-    results = calculate_degree_centrality(graph)
+    logging.info('Running calculate degree centrality for organism')
+    graph1 = json_graph.node_link_graph(graph)
+    results = calculate_degree_centrality(graph1)
+    logging.info('End of calculate degree centrality for organism')
     for gene_locus_tag, value in results.items():        
         centrality = CalculateCentralityRegistry(locus_tag = Gene.objects.get(locus_tag = gene_locus_tag), 
                                                  degree = value)
         centrality.save()
     logging.info('End of calculate degree centrality for organism %s' % organism_accession)
+    return graph
 
 @shared_task(bind=True, name='run_closeness_centrality', base=FunRegulationBaseTask)
 def task_closeness_centrality(self, graph, registry_id, organism_accession):
@@ -53,23 +54,57 @@ def task_closeness_centrality(self, graph, registry_id, organism_accession):
     registry.task_calculate_closeness = TaskResult.objects.get(task_id=self.request.id)
     registry.save()
     logging.info('Running calculate closeness centrality for organism %s' % organism_accession)
-    graph = json_graph.node_link_graph(graph)
-    results = calculate_closeness_centrality(graph)
+    graph1 = json_graph.node_link_graph(graph)
+    results = calculate_closeness_centrality(graph1)
+    logging.info('End of calculate closeness centrality for organism %s' % organism_accession)
     for gene_locus_tag, value in results.items():
         centrality = CalculateCentralityRegistry(locus_tag = Gene.objects.get(locus_tag = gene_locus_tag), 
                                                  closeness = value)
-        centrality.save()
+        centrality.save(update_fields=['closeness'])
     logging.info('End of calculate closeness centrality for organism %s' % organism_accession)
-
+    return graph
 
 @shared_task(bind=True, name='run_betweenness_centrality', base=FunRegulationBaseTask)
-def task_betweenness_centrality(self, graph, organism_accession):
-    pass
+def task_betweenness_centrality(self, graph, registry_id, organism_accession):
+    registry = ProjectAnalysisRegistry.objects.get(pk=registry_id)
+    registry.task_calculate_betweenness = TaskResult.objects.get(task_id=self.request.id)
+    registry.save()
+    logging.info('Running calculate betweenness centrality for organism %s' % organism_accession)
+    graph1 = json_graph.node_link_graph(graph)
+    results = calculate_betweenness_centrality(graph1)
+    for gene_locus_tag, value in results.items():
+        centrality = CalculateCentralityRegistry(locus_tag = Gene.objects.get(locus_tag = gene_locus_tag), 
+                                                betweenness = value)
+        centrality.save(update_fields=['betweenness'])
+    logging.info('End of calculate betweenness centrality for organism %s' % organism_accession)
+    return graph
 
 @shared_task(bind=True, name='run_eigenvector_centrality', base=FunRegulationBaseTask)
-def task_eigenvector_centrality(self, graph, organism_accession):
-    pass
+def task_eigenvector_centrality(self, graph, registry_id, organism_accession):
+    registry = ProjectAnalysisRegistry.objects.get(pk=registry_id)
+    registry.task_calculate_eigenvector = TaskResult.objects.get(task_id=self.request.id)
+    registry.save()
+    logging.info('Running calculate eigenvector centrality for organism %s' % organism_accession)
+    graph1 = json_graph.node_link_graph(graph)
+    results = calculate_eigenvector_centrality(graph1)
+    for gene_locus_tag, value in results.items():
+        centrality = CalculateCentralityRegistry(locus_tag = Gene.objects.get(locus_tag = gene_locus_tag), 
+                                                 eigenvector = value)
+        centrality.save(update_fields=['eigenvector'])
+    logging.info('End of calculate eigenvector centrality for organism %s' % organism_accession)
+    return graph
 
 @shared_task(bind=True, name='run_harmonic_centrality', base=FunRegulationBaseTask)
-def task_harmonic_centrality(self, graph, organism_accession):
-    pass
+def task_harmonic_centrality(self, graph, registry_id, organism_accession):
+    registry = ProjectAnalysisRegistry.objects.get(pk=registry_id)
+    registry.task_calculate_harmonic = TaskResult.objects.get(task_id=self.request.id)
+    registry.save()
+    logging.info('Running calculate harmonic centrality for organism %s' % organism_accession)
+    graph1 = json_graph.node_link_graph(graph)
+    results = calculate_harmonic_centrality(graph1)
+    for gene_locus_tag, value in results.items():
+        centrality = CalculateCentralityRegistry(locus_tag = Gene.objects.get(locus_tag = gene_locus_tag), 
+                                                 harmonic = value)
+        centrality.save(update_fields=['harmonic'])
+    logging.info('End of calculate harmonic centrality for organism %s' % organism_accession)
+    return graph
--- funRegulationBackCode/projects/tasks_chain.py
+++ funRegulationBackCode/projects/tasks_chain.py
@@ -8,6 +8,7 @@ from django_celery_results.models import TaskResult
 from funRegulationTool.task_utils import FunRegulationBaseTask
 from projects.tasks_import import task_import_genes
 from projects.tasks_external_tools import task_run_proteinortho, task_run_rsat
+from projects.tasks_calc_centrality import *
 from api.models import ProjectAnalysisRegistry
 
 def analyse_registry(registry):
@@ -22,12 +23,25 @@ def task_analyse_registry(self, registry_id):
     registry = ProjectAnalysisRegistry.objects.get(pk=registry_id)
     with transaction.atomic():
         if registry.download_organism and registry.proteinortho_analyse and registry.rsat_analyse:
+            #chain(task1.s(),chord([task2.s(), task3.s()], immutable=True),task4.s()).delay()
             chain(task_import_genes.si(registry_id, registry.organism_accession) | 
-                  task_run_proteinortho.si(registry_id, registry.organism_accession) | 
+                  task_run_proteinortho.si(registry_id, registry.organism_accession) |
+                  task_run_create_graph.si(registry_id, registry.organism_accession) |
+                  task_degree_centrality.s(registry_id, registry.organism_accession) | 
+                  task_closeness_centrality.s(registry_id, registry.organism_accession) | 
+                  task_betweenness_centrality.s(registry_id, registry.organism_accession) | 
+                  task_eigenvector_centrality.s(registry_id, registry.organism_accession) | 
+                  task_harmonic_centrality.s(registry_id, registry.organism_accession) | 
                   task_run_rsat.si(registry_id, registry.organism_accession)
                   ).apply_async()
         elif registry.download_organism and registry.proteinortho_analyse:
             chain(task_import_genes.si(registry_id, registry.organism_accession) |
-                  task_run_proteinortho.si(registry_id, registry.organism_accession)
+                  task_run_proteinortho.si(registry_id, registry.organism_accession) |
+                  task_run_create_graph.si(registry_id, registry.organism_accession) |
+                  task_degree_centrality.s(registry_id, registry.organism_accession) | 
+                  task_closeness_centrality.s(registry_id, registry.organism_accession) | 
+                  task_betweenness_centrality.s(registry_id, registry.organism_accession) | 
+                  task_eigenvector_centrality.s(registry_id, registry.organism_accession) | 
+                  task_harmonic_centrality.s(registry_id, registry.organism_accession)
                  ).apply_async()
         registry.save()
